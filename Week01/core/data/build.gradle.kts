cc8581426250c2a61bc51c187ae8478a3df0fa9e
moondev03 <moondev03@gmail.com>
2025-10-30 17:34:38 +0900

    build(data): Retrofit, OkHttp, Serialization 라이브러리 추가

--- Week01/core/data/build.gradle.kts
+++ Week01/core/data/build.gradle.kts
@@ -45,10 +45,10 @@ dependencies {
     implementation(libs.hilt.android)
     kapt(libs.hilt.compiler)
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.kotlin.serialization)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.okhttp.logging)
+
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
