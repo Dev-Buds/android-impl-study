6f8c0986ac87886b5478f96795417edb6521a6cc
moondev03 <moondev03@gmail.com>
2025-10-31 15:27:04 +0900

    feat: LocalDateTime 직렬화/역직렬화 구현

---
+++ Week01/core/data/src/main/java/com/example/data/util/LocalDateTimeIsoSerializer.kt
@@ -0,0 +1,29 @@
+package com.example.data.util
+
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+object LocalDateTimeIsoSerializer : KSerializer<LocalDateTime> {
+    private val formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME
+
+    override val descriptor: SerialDescriptor =
+        PrimitiveSerialDescriptor("LocalDateTime", PrimitiveKind.STRING)
+
+    override fun serialize(
+        encoder: Encoder,
+        value: LocalDateTime,
+    ) {
+        encoder.encodeString(value.format(formatter))
+    }
+
+    override fun deserialize(decoder: Decoder): LocalDateTime {
+        val value = decoder.decodeString()
+        return LocalDateTime.parse(value, formatter)
+    }
+}
