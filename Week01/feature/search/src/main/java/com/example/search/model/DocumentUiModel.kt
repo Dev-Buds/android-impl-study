829660e6ce2f6e27fdc04c7ec9987150b93fc4f7
moondev03 <moondev03@gmail.com>
2025-11-04 03:27:03 +0900

    feat: 북마크 기능 관련 DocumentUiModel 수정

--- Week01/feature/search/src/main/java/com/example/search/model/DocumentUiModel.kt
+++ Week01/feature/search/src/main/java/com/example/search/model/DocumentUiModel.kt
@@ -1,5 +1,6 @@
 package com.example.search.model
 
+import com.example.domain.model.BookmarkItem
 import com.example.domain.model.ImageDocument
 import com.example.domain.model.VClipDocument
 import java.time.LocalDateTime
@@ -7,15 +8,31 @@ import java.time.LocalDateTime
 sealed interface DocumentUiModel {
     val datetime: LocalDateTime
     val isBookmarked: Boolean
+    val id: String
+
+    fun toBookmark(): BookmarkItem
+
+    fun updateBookmarkState(isBookmarked: Boolean): DocumentUiModel
 
     data class ImageDocumentUiModel(
-        override val datetime: LocalDateTime,
-        override val isBookmarked: Boolean = false,
         val collection: String,
         val docUrl: String,
         val thumbnailUrl: String,
         val imageUrl: String,
+        override val datetime: LocalDateTime,
+        override val isBookmarked: Boolean = false,
+        override val id: String = "${thumbnailUrl}_$datetime",
     ) : DocumentUiModel {
+        override fun updateBookmarkState(isBookmarked: Boolean): DocumentUiModel = copy(isBookmarked = isBookmarked)
+
+        override fun toBookmark(): BookmarkItem =
+            BookmarkItem(
+                id = id,
+                url = docUrl,
+                thumbnailUrl = thumbnailUrl,
+                dateTime = datetime,
+            )
+
         companion object {
             fun from(document: ImageDocument) =
                 ImageDocumentUiModel(
@@ -29,12 +46,23 @@ sealed interface DocumentUiModel {
     }
 
     data class VClipDocumentUiModel(
-        override val datetime: LocalDateTime,
-        override val isBookmarked: Boolean = false,
         val url: String,
         val thumbnail: String,
         val author: String,
+        override val datetime: LocalDateTime,
+        override val isBookmarked: Boolean = false,
+        override val id: String = "${thumbnail}_$datetime",
     ) : DocumentUiModel {
+        override fun updateBookmarkState(isBookmarked: Boolean): DocumentUiModel = copy(isBookmarked = isBookmarked)
+
+        override fun toBookmark(): BookmarkItem =
+            BookmarkItem(
+                id = id,
+                url = url,
+                thumbnailUrl = thumbnail,
+                dateTime = datetime,
+            )
+
         companion object {
             fun from(document: VClipDocument) =
                 VClipDocumentUiModel(
